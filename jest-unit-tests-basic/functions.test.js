a92d545bdfce003667d24827f0f20d449de9daf2
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-14 13:56:19 +0100

    Jest - asyns functions tests

--- jest-unit-tests-basic/functions.test.js
+++ jest-unit-tests-basic/functions.test.js
@@ -37,3 +37,17 @@ test('Admin should be in usernames', () => {
     usernames = ['john', 'bob', 'lisa', 'admin'];
     expect(usernames).toContain('admin');
 });
+
+test('User fetched name should be Leanne Graham', () => {
+    expect.assertions(1);
+    return functions.fetchUser()
+        .then(data => {
+            expect(data.name).toEqual('Leanne Graham');
+    });
+});
+
+test('User fetched name should be Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+});
