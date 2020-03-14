7f689b1ce954739d0c2919fd3fc13c99c2e46729
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-14 17:01:33 +0100

    Higher order functions - arraySum - functional approach

--- higher-order-functions/arrayfunctions.js
+++ higher-order-functions/arrayfunctions.js
@@ -1,10 +1,15 @@
 // Task
 // arraySum: (arr: number[]) => number
 // returns sum of all elements of the array and 0 if the array is empty
-function arraySum(arr) {
+
+/*function arraySum(arr) {
   let sum = 0;
   arr.forEach((el) => sum += el);
   return sum;
+}*/
+
+function arraySum(arr) {
+  return arr.reduce((total, el) => total + el, 0);
 }
 
 const arrayFunctions = {
