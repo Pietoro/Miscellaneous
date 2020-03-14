685794a97033ec1cc1866bea9d149afd1a994c18
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-14 16:41:44 +0100

    Higher order functions - TDD - arraySum

--- higher-order-functions/arrayfunctions.test.js
+++ higher-order-functions/arrayfunctions.test.js
@@ -1,3 +1,13 @@
 const arrayFunctions = require('./arrayfunctions');
 const arraySum = arrayFunctions.arraySum; // const { arraySum } = arrayFunctions;
 
+test('Should return sum of all array elements', () => {
+  const array1 = [1,2,3];
+  expect(arraySum(array1)).toBe(6);
+
+  const array2 = [1,-2,0];
+  expect(arraySum(array2)).toBe(-1);
+  
+  const array2 = [];
+  expect(arraySum(array2)).toBe(0);
+});
