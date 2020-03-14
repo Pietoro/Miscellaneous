c5730f852e0890f29d231b19ed022fba66f04017
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-14 13:37:58 +0100

    Jest - tests for functions.js

--- jest-unit-tests-basic/functions.test.js
+++ jest-unit-tests-basic/functions.test.js
@@ -3,3 +3,37 @@ const functions = require('./functions');
 test('The sum of 2 and 2 is equal to 4', () => {
     expect(functions.add(2,2)).toBe(4);
 });
+
+test('The sum of 2 and 2 is not equal to 5', () => {
+    expect(functions.add(2,2)).not.toBe(5);
+});
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test('User should be Slav Kusinski', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Slav',
+        lastName: 'Kusinski'
+    });
+});
+
+test('Should be under 5000', () => {
+    const load1 = 1000;
+    const load2 = 3000;
+    expect(functions.add(load1,load2)).toBeLessThan(5000);
+});
+
+test('There is no "I" in "team"', () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test('Admin should be in usernames', () => {
+    usernames = ['john', 'bob', 'lisa', 'admin'];
+    expect(usernames).toContain('admin');
+});
