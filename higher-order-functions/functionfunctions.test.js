6e752346cead0b9efa8c7c000081335f6a57bf10
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-21 12:57:26 +0100

    Higher order functions - TDD - constFunc

---
+++ higher-order-functions/functionfunctions.test.js
@@ -0,0 +1,14 @@
+import each from "jest-each"
+
+
+describe('Testing constFunc',() => {
+  each([
+    [{c: 3, xs: [2, 'a', true]}],
+    [{c: true, xs: ['abbc',[1,2],false]}]
+  ]).test('Should return a constant function', (input) => {
+    const func = constFunc(input.c);
+    input.xs.forEach(x => {
+      expect(func(x)).toBe(input.c);
+    });
+  });
+});
