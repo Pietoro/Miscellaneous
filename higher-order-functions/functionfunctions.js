d80abfef9daa42908f9d3d78dc9a93be846f6a8e
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-21 13:03:23 +0100

    Higher order functions - constFunc - implementation

--- higher-order-functions/functionfunctions.js
+++ higher-order-functions/functionfunctions.js
@@ -1,3 +1,7 @@
 // Task
 // constFunc: (c: any) => ((x: any) => any)
 // returns a function that returns always c, no matter what argument it is given
+
+export const constFunc = (c) => function(x) {
+    return c;
+};
--- higher-order-functions/functionfunctions.test.js
+++ higher-order-functions/functionfunctions.test.js
@@ -1,5 +1,5 @@
-import each from "jest-each"
-
+import each from "jest-each";
+import {constFunc} from './functionfunctions';
 
 describe('Testing constFunc',() => {
   each([
