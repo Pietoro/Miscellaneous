81859a3bb810d5d1beb496093da7f47a1cf2fb34
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-23 21:51:11 +0100

    Sorting algorithms - measurements - testSortingAlgorithms

--- sorting-algorithms/measurements.js
+++ sorting-algorithms/measurements.js
@@ -1,4 +1,25 @@
+import sortingAlgorithms from './sortingAlgorithms.js'
+
+// originally implemented and tested in higher-order-functions/functionfunctions
+const timeIt = (func) => ((x) => {
+  const start = new Date().getTime();
+  const result = func(x); 
+  const finish = new Date().getTime();
+  const time = finish - start;
+  return {result, time};
+});
+
 const randomArray = (length, range = 500) => 
   Array.from({length: length})
     .map((x) => Math.random() * 2 * range - range);
+
+const testSortingAlgorithm = (algorithm,length,times = 1,range = 500) => {
+  const timedAlgorithm = timeIt(algorithm);
+  let time = 0;
+  for(let i = 1;i <= times;i++) {
+    time += timedAlgorithm(randomArray(length,range)).time;
+  }
+  return time/times;
+};
+
 
