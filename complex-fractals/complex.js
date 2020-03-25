c4d8383df04ba542e420bb4bf98585ee5be15142
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-25 19:33:54 +0100

    Complex fractals - mul

--- complex-fractals/complex.js
+++ complex-fractals/complex.js
@@ -9,3 +9,6 @@ export const add = (z1, z2) =>
 
 export const sub = (z1, z2) => 
   ({re: (z1.re - z2.re), im: (z1.im - z2.im)});
+
+// mul: (z1: complex, z2: complex) => complex
+
--- complex-fractals/complex.test.js
+++ complex-fractals/complex.test.js
@@ -1,4 +1,4 @@
-import {add, sub} from './complex.js';
+import {add, sub, mul} from './complex.js';
 import each from 'jest-each';
 
 describe('Testing add', () => {
@@ -20,5 +20,17 @@ describe('Testing sub', () => {
     [{re: 5, im: 4}, {re: 0, im: 0}, {re: 5, im: 4}]
   ]).test('Should return the difference', (z1, z2, expected) => {
     expect(sub(z1,z2)).toEqual(expected);
+  });
+});
+
+describe('Testing mul', () => {
+  each([
+    [{re: 1, im: 2}, {re: 3, im: -1}, {re: 5, im: 5}],
+    [{re: -3, im: 4}, {re: 2, im: 0}, {re: -6, im: 8}],
+    [{re: 0, im: 0}, {re: 5, im: 4}, {re: 0, im: 0}],
+    [{re: 1, im: 0}, {re: 3, im: 2}, {re: 3, im: 2}]
+  ]).test('Should return the product', (z1, z2, expected) => {
+    expect(mul(z1,z2)).toEqual(expected);
+    expect(mul(z2,z1)).toEqual(mul(z1,z2));
   });
 });
