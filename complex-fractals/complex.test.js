06fc901626d6632f3f3074977e765e66c5824365
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-25 19:07:22 +0100

    Complex fractals - test

---
+++ complex-fractals/complex.test.js
@@ -0,0 +1,13 @@
+import {add} from './complex.js';
+import each from 'jest-each';
+
+describe('Testing add', () => {
+  each([
+    [{re: 1, im: 2}, {re: 3, im: -1}, {re:4, im: 1}],
+    [{re: -3, im: 4}, {re: 2, im: 0}, {re: -1, im: 4}],
+    [{re: 0, im: 0}, {re: 5, im: 4}, {re: 5, im: 4}]
+  ]).test('Should return the sum', (z1, z2, expected) => {
+    expect(add(z1,z2)).toEqual(expected);
+    expect(add(z2,z1)).toEqual(add(z1,z2));
+  });
+});
