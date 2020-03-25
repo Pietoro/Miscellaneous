f16e444ea04c54e89706e95ea28c5f1ca5bc84b3
Ewelina Kania <ewelina.kania93@gmail.com>
2020-03-25 19:19:35 +0100

    Complex fractals - sub

--- complex-fractals/complex.js
+++ complex-fractals/complex.js
@@ -4,3 +4,8 @@
 
 export const add = (z1, z2) => 
   ({re: (z1.re + z2.re), im: (z1.im + z2.im)});
+
+// sub: (z1: complex, z2: complex) => complex
+
+export const sub = (z1, z2) => 
+  ({re: (z1.re - z2.re), im: (z1.im - z2.im)});
--- complex-fractals/complex.test.js
+++ complex-fractals/complex.test.js
@@ -1,4 +1,4 @@
-import {add} from './complex.js';
+import {add, sub} from './complex.js';
 import each from 'jest-each';
 
 describe('Testing add', () => {
@@ -9,5 +9,16 @@ describe('Testing add', () => {
   ]).test('Should return the sum', (z1, z2, expected) => {
     expect(add(z1,z2)).toEqual(expected);
     expect(add(z2,z1)).toEqual(add(z1,z2));
+  });
+});
+
+describe('Testing sub', () => {
+  each([
+    [{re: 1, im: 2}, {re: 3, im: -1}, {re:-2, im: 3}],
+    [{re: -3, im: 4}, {re: 2, im: 0}, {re: -5, im: 4}],
+    [{re: 0, im: 0}, {re: 5, im: 4}, {re: -5, im: -4}],
+    [{re: 5, im: 4}, {re: 0, im: 0}, {re: 5, im: 4}]
+  ]).test('Should return the difference', (z1, z2, expected) => {
+    expect(sub(z1,z2)).toEqual(expected);
   });
 });
