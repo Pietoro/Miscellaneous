69b93b8e8c617b3d75d576d8f9866c3c62248e36
Ewelina Kania <ewelina.kania93@gmail.com>
2020-04-07 21:22:39 +0200

    Routing basics - spa - notifications finish

--- routing-basics/spa/popups/notifications.js
+++ routing-basics/spa/popups/notifications.js
@@ -1,17 +1,19 @@
-export default function showNotification(header, text) {
+export default function showNotification(header, text, displayTime = 5000) {
 
-  console.log('salut');
   
-  const notificationsBox = document.createElement('div');
-  notificationsBox.classList.add('notification-box');
+  const notificationBox = document.createElement('div');
+  notificationBox.classList.add('notification-box');
 
   const notificationHeader = document.createElement('h3');
   notificationHeader.appendChild(document.createTextNode(header));
   const notificationText = document.createTextNode(text);
   
-  notificationsBox.appendChild(notificationHeader);
-  notificationsBox.appendChild(notificationText);
+  notificationBox.appendChild(notificationHeader);
+  notificationBox.appendChild(notificationText);
   const notificationContainer =   document.getElementById('notifications');
-  notificationContainer.insertBefore(notificationsBox, notificationContainer.firstChild);
+  notificationContainer.insertBefore(notificationBox, notificationContainer.firstChild);
 
+  setTimeout(() => notificationContainer.removeChild(notificationBox), displayTime);
+  const fadeOutTime = displayTime / 2;
+  setTimeout(() => notificationBox.style.opacity = '30%', fadeOutTime);
 }
