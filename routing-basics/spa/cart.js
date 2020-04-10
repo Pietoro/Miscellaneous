61aed153405af2ffa855384b166a709c823522c7
Ewelina Kania <ewelina.kania93@gmail.com>
2020-04-10 20:04:21 +0200

    Routing basics - spa - cart new features

--- routing-basics/spa/cart.js
+++ routing-basics/spa/cart.js
@@ -3,7 +3,14 @@ import showNotification from './popups/notifications.js'
 export function addToCart(product) {
 
   let cartContent = JSON.parse(localStorage.getItem('cartContent'));
-  cartContent = [...cartContent, {count: 1, product}];
+  
+  const index = cartContent.findIndex((content) => content.product.id === product.id);
+  if(index > -1) {
+    cartContent[index].count += 1;
+  } else {
+    cartContent = [...cartContent, {count: 1, product}];
+  }
+
   localStorage.setItem('cartContent', JSON.stringify(cartContent));
 
   showNotification('Udało się',product.city);
