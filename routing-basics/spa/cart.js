2de973eac1b1d87e40637e793f34255e1dc446d7
Ewelina Kania <ewelina.kania93@gmail.com>
2020-04-13 12:18:16 +0200

    Routing basics - spa - cart functionality

--- routing-basics/spa/cart.js
+++ routing-basics/spa/cart.js
@@ -24,3 +24,45 @@ export function clearCart() {
 export function getCartContent() {
   return JSON.parse(localStorage.getItem('cartContent'));
 }
+
+export function removeFromCart(product) {
+
+  let cartContent = JSON.parse(localStorage.getItem('cartContent'));
+  
+  const index = cartContent.findIndex((content) => content.product.id === product.id);
+  if(index > -1) {
+    cartContent.splice(index, 1);
+  } 
+
+  localStorage.setItem('cartContent', JSON.stringify(cartContent));
+}
+
+export function decreaseCountInCart(product) {
+
+  let cartContent = JSON.parse(localStorage.getItem('cartContent'));
+  
+  const index = cartContent.findIndex((content) => content.product.id === product.id);
+  if(index > -1) {
+    cartContent[index].count -= 1;
+    if(cartContent[index].count <= 0) {
+      removeFromCart(product);
+    } else {
+      localStorage.setItem('cartContent', JSON.stringify(cartContent));
+    }
+  }
+}
+
+export function increaseCountInCart(product) {
+
+  let cartContent = JSON.parse(localStorage.getItem('cartContent'));
+  
+  const index = cartContent.findIndex((content) => content.product.id === product.id);
+  if(index > -1) {
+    cartContent[index].count += 1;
+    if(cartContent[index].count <= 0) {
+      removeFromCart(product);
+    } else {
+      localStorage.setItem('cartContent', JSON.stringify(cartContent));
+    }
+  }
+}
--- routing-basics/spa/views/cart/index.js
+++ routing-basics/spa/views/cart/index.js
@@ -1,5 +1,7 @@
 import {getPrices} from '../shop/index.js';
-import { clearCart, getCartContent } from '../../cart.js';
+import { clearCart, getCartContent, removeFromCart, decreaseCountInCart, increaseCountInCart } from '../../cart.js';
+import {showConfirm} from '../../popups/alerts.js';
+import showNotification from '../../popups/notifications.js'
 
 let cartContent = [
   // {
@@ -58,27 +60,39 @@ function fullCart(article) {
     <table class="cart-table" id="cart-table">
       <thead>
         <tr>
-          <th>Product</th>
-          <th>Amount</th>
-          <th>Price</th>
-          <th>Total price</th>
+          <th></th>
+          <th class="head-product">Product</th>
+          <th class="head-amount">Amount</th>
+          <th class="head-price">Price</th>
+          <th class="head-total">Total price</th>
         </tr>
       </thead>
       <tbody>
       
       </tbody>
     </table>
-    <button class="btn-cart clear-cart" id="clear-cart">Clear cart</button>
-    <button class="btn-cart continue-cart">Continue shopping</button>
-    <button class="btn-cart buy-cart">Buy</button>
+    <button class="btn-cart" id="btn-clear-cart">Clear cart</button>
+    <button class="btn-cart" id="btn-continue-shopping" onclick="link('shop','index')">Continue shopping</button>
+    <button class="btn-cart" id="btn-cart-buy">Buy</button>
   `;
 
   fillCartTable();
-  document.getElementById('clear-cart').onclick = () => {
+  document.getElementById('btn-clear-cart').onclick = () => {
     clearCart();
     emptyCart(article);
   };
   
+  document.getElementById('btn-cart-buy').onclick = () => {
+    showConfirm(
+      'Product purchase',
+      'Are you sure you want buy this product?', 
+      () => {
+        showNotification('Product purchase',`Thank you for buying our products`)
+        clearCart();
+        emptyCart(article);
+      }
+    );
+  };
 }
 
 async function fillCartTable() {
@@ -95,26 +109,60 @@ async function fillCartTable() {
     )));
 
   document.getElementById('cart-table').querySelector('tbody').innerHTML = cartTableData
-    .map((content) => `
+    .map((content, index) => `
       <tr>
+        <td><button class="link btn-cart-rmv" id="btn-remove-${index}">x</button></td>
         <td><b>${content.product.city}</b> by ${content.product.name}</td>
-        <td>${content.count}</td>
+        <td>${content.count}<button class="link btn-cart-change" id="btn-decr-${index}">-</button><button class="link btn-cart-change" id="btn-incr-${index}">+</button></td>
         <td>${content.product.price}$</td>
-        <td>${content.product.price * content.count}$</td>
+        <td>${(content.product.price * content.count).toFixed(2)}$</td>
       </tr>
     `)
-    .reduce((total, el) => total.concat(el));
+    .reduce((total, el) => total.concat(el), '');
 
   document.getElementById('cart-table').querySelector('tbody').innerHTML += 
     `<tr>
-    <td></td><td></td><td></td>
+    <td></td><td></td><td></td><td></td>
     <td><b>
     ${cartTableData
       .map((content) => parseFloat(content.product.price) * content.count)
       .reduce((total, el) => total + el, 0)
+      .toFixed(2)
       }$
     </b></td>
    </tr>`
+
+   cartTableData.forEach((content, index) => {
+    console.log(content);
+    console.log(index);
+    document.getElementById(`btn-remove-${index}`).onclick = () => {
+      removeFromCart(content.product);
+      cartContent = getCartContent();
+      fillCartTable();
+
+      if(cartContent.length === 0) {
+        emptyCart(document.getElementById('view-cart-index'));
+      } 
+    };
+    document.getElementById(`btn-decr-${index}`).onclick = () => {
+      decreaseCountInCart(content.product);
+      cartContent = getCartContent();
+      fillCartTable();
+
+      if(cartContent.length === 0) {
+        emptyCart(document.getElementById('view-cart-index'));
+      } 
+    };
+    document.getElementById(`btn-incr-${index}`).onclick = () => {
+      increaseCountInCart(content.product);
+      cartContent = getCartContent();
+      fillCartTable();
+
+      if(cartContent.length === 0) {
+        emptyCart(document.getElementById('view-cart-index'));
+      } 
+    };
+  });
 }
 
 
