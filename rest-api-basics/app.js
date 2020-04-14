48f62dc5146a31d437b4186ed0dbf6bfde8e8f94
Ewelina Kania <ewelina.kania93@gmail.com>
2020-04-14 21:31:15 +0200

    Rest API basics - new route and middlewares

--- rest-api-basics/app.js
+++ rest-api-basics/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
+
+// Middlewares
+
+// app.use('/posts', (request, response, next) => {
+//   console.log('This is a middleware');
+//   next();
+// });
+
 // ROUTES
 
 app.get('/', (request, response) => {
   response.send('We are on home');
 });
 
-app.listen(PORT)
+app.get('/posts', (request, response) => {
+  response.send('We are on posts');
+});
+
+app.listen(PORT);
